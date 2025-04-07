f70cb981117b16e8ecf3a7191834c09e96f2723e
SrijanSriv211 <QCoreNest@gmail.com>
2025-04-07 17:38:44 +0530

    Create utils.py

---
+++ src/shared/utils.py
@@ -0,0 +1,42 @@
+import torch, pickle, time, sys, os
+
+# dprint -> delay print
+# ChatGPT like print effect.
+def dprint(text, delay=0.001):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(delay)
+    print("\n")
+
+def get_bin_path():
+    # get the absolute path of the current file
+    current_file_path = os.path.abspath(__file__)
+
+    # navigate up the directory tree four times
+    parent_dir = current_file_path
+    rel_current_path = os.path.relpath(__file__)
+    for _ in range(len(rel_current_path.split(os.sep))):
+        parent_dir = os.path.dirname(parent_dir)
+
+    # append "cache" directory to the parent directory
+    return os.path.join(parent_dir, "bin")
+
+def calc_total_time(seconds):
+    # separate the integer part (for hours, minutes, and seconds) from the fractional part (for milliseconds)
+    sec_int, millis = divmod(seconds, 1)
+    millis = int(millis * 1000) # convert the fractional part to milliseconds
+
+    min, sec = divmod(int(sec_int), 60)
+    hour, min = divmod(min, 60)
+    hours, minutes, seconds = int(hour), int(min), int(sec)
+
+    t = [
+        f"{hours} hour" + ("s" if hours > 1 else "") if hours > 0 else None,
+        f"{minutes} minute" + ("s" if minutes > 1 else "") if minutes > 0 else None,
+        f"{seconds} second" + ("s" if seconds > 1 else "") if seconds > 0 else None,
+        f"{millis} ms" if millis > 0 else None
+    ]
+    t = list(filter(None, t))
+
+    return ", ".join(t) if t else "0 seconds"
