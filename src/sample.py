8157a95c6c7e615ad1b222c22726e628a0e695cb
SrijanSriv211 <QCoreNest@gmail.com>
2025-04-07 23:45:21 +0530

    Create sample.py

---
+++ src/sample.py
@@ -0,0 +1,42 @@
+from colorama import Style, Fore, init
+from encoder.bytepair import Encoder
+from models.gpt import sample
+import warnings, torch, sys
+
+init(autoreset = True)
+
+# supress pytorch's future warning:
+# You are using `torch.load` with `weights_only=False` (the current default value), which uses the default pickle module implicitly.
+# It is possible to construct malicious pickle data which will execute arbitrary code during unpickling
+# (See https://github.com/pytorch/pytorch/blob/main/SECURITY.md#untrusted-models for more details).
+# In a future release, the default value for `weights_only` will be flipped to `True`.
+# This limits the functions that could be executed during unpickling.
+# Arbitrary objects will no longer be allowed to be loaded via this mode unless they are explicitly allowlisted by the user via `torch.serialization.add_safe_globals`.
+# We recommend you start setting `weights_only=True` for any use case where you don't have full control of the loaded file.
+# Please open an issue on GitHub for any issues related to this experimental feature.
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+s = sample()
+s.load(torch.load(sys.argv[1]), True)
+
+enc = Encoder()
+enc.load("bin\\cl5k.bin")
+
+test = [
+	"Google ",
+	"Shahrukh Khan ",
+	"Hello I'm a language model, and ",
+	"Can I say that Calcia is really a branch of math or is it something nonsense",
+	"Every year the moon is going",
+	"o/ The workings of the Undetailed",
+	"Hi!\n<|sot|>",
+	"Hello buddy.\n<|sot|>",
+	None,
+	None,
+	None
+]
+
+for txt in test:
+	enctxt = enc.encode(txt, allowed_special="all") if txt != None else txt
+	out = enc.decode(s.generate(enctxt, length=256))
+	print(f"{Style.BRIGHT}{Fore.BLACK}```\n{out}\n```\n")
