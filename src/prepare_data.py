13b3a2f256250676801806c46cd0899f57c2cc8d
Srijan Srivastava <QCoreNest@gmail.com>
2025-05-15 02:22:37 +0530

    finally it works great!!

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -2,7 +2,7 @@ from encoder.bytepair import Encoder
 from rich.progress import track
 from colorama import Style, Fore, init
 from pathlib import Path
-import argparse, numpy, json, os
+import itertools, argparse, numpy, json, os
 
 init(autoreset=True)
 
@@ -11,7 +11,8 @@ parser.add_argument("-i", help="dataset path", required=True)
 parser.add_argument("-o", help="output path", required=True)
 parser.add_argument("-e", help="encoder path", required=True)
 parser.add_argument("-d", help="train-val data division ratio", type=float, default=0.9)
-parser.add_argument("-s", help="max chars in each data shard", type=int, default=2_000_000_000)
+parser.add_argument("-s", help="max toks in each data shard", type=int, default=50_000_000)
+parser.add_argument("-c", help="context length", type=int, default=1024)
 args = parser.parse_args()
 
 CONFIG = {
@@ -19,7 +20,8 @@ CONFIG = {
 	"outpath": args.o,
 	"enc_path": args.e,
 	"data_division": args.d,
-	"chars_per_shard": args.s
+	"toks_per_shard": args.s,
+	"block_size": args.c
 }
 
 """
@@ -40,38 +42,42 @@ total_unique_chars, total_chars = 0, 0
 total_train_chars, total_val_chars = 0, 0
 total_train_tokens, total_val_tokens = 0, 0
 lensum = lambda x: sum([len(i) for i in x])
-save_file_idx = [0, 0]
 
 def save_data(data, split, file):
-	# if not os.path.isdir(os.path.join(CONFIG["outpath"], split)):
-	# 	os.mkdir(os.path.join(CONFIG["outpath"], split))
+	if not os.path.isdir(os.path.join(CONFIG["outpath"], split)):
+		os.mkdir(os.path.join(CONFIG["outpath"], split))
 
-	if split == "train": save_file_idx[0] += 1
-	else: save_file_idx[1] += 1
+	id = 1
+	jump = int(CONFIG["toks_per_shard"]/CONFIG["block_size"])
+	num_shards = int(lensum(data)/CONFIG["toks_per_shard"]) + 1
 
-	idx = save_file_idx[0] if split == "train" else save_file_idx[1]
-	outpath = f"{CONFIG["outpath"]}/{split}/{Path(file).stem}" + (f"_{idx}" if idx > 0 else "") + ".bin"
-	print(outpath)
+	for i in range(num_shards):
+		outpath = f"{CONFIG["outpath"]}/{split}/{Path(file).stem}" + (f"_{id}" if num_shards > 1 else "") + ".bin"
+		id += 1
 
-	print(lensum(data))
-	pad = len(max(data, key=len))
-	data = numpy.array([i + [-1]*(pad-len(i)) for i in data])
-	print(lensum(data))
-	# data.tofile(outpath)
+		print(outpath)
+		numpy.array(data[i * jump : (i+1) * jump], dtype=numpy.int16).tofile(outpath)
 
-def encode_data(data, split, file):
-	global save_file_idx
+# given an iterable of iterables `lists`, emit successive chunks of length `size`,
+# flattening across sub-lists, and pad the final chunk with `fill` if necessary.
+def post_process_data(lst, size, fill=-1):
+    # make one big iterator over all values
+    flat_iter = itertools.chain.from_iterable(lst)
+    # repeatedly grab 'size' items until the iterator is exhausted
+    for chunk in iter(lambda: list(itertools.islice(flat_iter, size)), []):
+        # rf the last chunk is shorter, pad it out
+        if len(chunk) < size:
+            chunk.extend([fill] * (size - len(chunk)))
+        yield chunk
+
+def encode_data(data, split):
 	num_chars = lensum(data)
 
 	for i, x in enumerate(track(data, f"{Fore.WHITE}{Style.BRIGHT}encoding {Fore.WHITE}{Style.DIM}{split} chars{Style.RESET_ALL}")):
-		data[i] = enc.encode(x, allowed_special="all")
+		data[i] = enc.encode(x + "\n\n", allowed_special="all")
 
-	num_tokens = lensum(data)
-	print(f"{(num_chars/1e6)}M {split} chars,", f"{(num_tokens/1e6)}M {split} tokens")
-
-	# save
-	save_data(data, split, file)
-	return num_chars, num_tokens
+	print(f"{(num_chars/1e6)}M {split} chars,", f"{(lensum(data)/1e6)}M {split} tokens")
+	return num_chars
 
 # split train and val data based on `data_division`
 def split_data(train_data):
@@ -101,40 +107,34 @@ for file in dataset_files:
 	# get total number chars and total number of unique chars
 	unique_chars = len(set().union(*set().union(*train_data)))
 	num_chars = lensum(train_data)
-	save_file_idx = [0, 0]
 
 	# verbose
 	total_chars += num_chars
 	total_unique_chars += unique_chars
 	print(f"{(num_chars/1e6)}M total chars,", f"{unique_chars} unique chars")
 
-	# load data in chunks
-	size = 0
-	chunk_ranges = [0]
-	for i, x in enumerate(train_data):
-		size += len(x)
-		if size > CONFIG["chars_per_shard"] or i+1 >= len(train_data):
-			chunk_ranges.append(i+1)
-			size = 0
-	chunk_ranges = list(zip(chunk_ranges, chunk_ranges[1:]))
+	# split train and val data
+	val_data = split_data(train_data)
+
+	# encode and post-process train data
+	num_train_chars = encode_data(train_data, "train")
+	train_data = list(post_process_data(train_data, CONFIG["block_size"]))
+	num_train_tokens = lensum(train_data)
+	save_data(train_data, "train", file)
 	del train_data
 
-	# now prepare data
-	for i in chunk_ranges:
-		with open(file, "r", encoding="utf-8") as f:
-			train_data = json.load(f)[i[0]:i[1]]
+	# encode and post-process val data
+	num_val_chars = encode_data(val_data, "val")
+	val_data = list(post_process_data(val_data, CONFIG["block_size"]))
+	num_val_tokens = lensum(val_data)
+	save_data(val_data, "val", file)
+	del val_data
+	print()
 
-		# split and save
-		val_data = split_data(train_data)
-		num_train_chars, num_train_tokens = encode_data(train_data, "train", file)
-		num_val_chars, num_val_tokens = encode_data(val_data, "val", file)
-		print()
-
-		total_train_chars += num_train_chars
-		total_train_tokens += num_train_tokens
-		total_val_chars += num_val_chars
-		total_val_tokens += num_val_tokens
-		del train_data, val_data
+	total_train_chars += num_train_chars
+	total_train_tokens += num_train_tokens
+	total_val_chars += num_val_chars
+	total_val_tokens += num_val_tokens
 
 print(f"{(total_chars/1e6)}M total chars,", f"{total_unique_chars} total unique chars")
 print(f"{(total_train_chars/1e6)}M total train chars" + f", {(total_val_chars/1e6)}M total val chars")
