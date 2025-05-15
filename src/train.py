b9632942c473e0b3da0c78422e8d6f1376b53a07
Srijan Srivastava <QCoreNest@gmail.com>
2025-05-16 04:27:10 +0530

    smol change

--- src/train.py
+++ src/train.py
@@ -303,11 +303,10 @@ while training_loop:
 		optimizer.zero_grad(set_to_none=True)
 
 		# timing and logging
+		t1 = time.time()
+		dt = t1 - t0
+		t0 = t1
 		if stats["iter_num"] % CONFIG["log_interval"] == 0:
-			t1 = time.time()
-			dt = t1 - t0
-			t0 = t1
-
 			# get loss as float. note: this is a CPU-GPU sync point
 			# scale up to undo the division above, approximating the true total loss (exact would have been a sum)
 			lossf = loss.item() * CONFIG["gradient_accumulation_steps"]
